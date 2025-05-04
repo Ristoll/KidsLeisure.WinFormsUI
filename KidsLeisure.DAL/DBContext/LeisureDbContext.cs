b9166062e343546f726aac5ba59d23fb2fb9a4d8
Ristoll <chamata.kristina@gmail.com>
2025-05-04 13:49:08 +0300

    Створено класи контексту БД. Підключено Microsoft.EntityFrameworkCore.SqlServer.

---
+++ KidsLeisure.DAL/DBContext/LeisureDbContext.cs
@@ -0,0 +1,48 @@
+﻿using KidsLeisure.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using KidsLeisure.DAL.Configurations;
+
+namespace KidsLeisure.DAL.DBContext
+{
+    public class LeisureDbContext : DbContext
+    {
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<ZoneEntity> Zones { get; set; }
+        public DbSet<AttractionEntity> Attractions { get; set; }
+        public DbSet<CharacterEntity> Characters { get; set; }
+
+        public DbSet<OrderZoneEntity> OrderZones { get; set; }
+        public DbSet<OrderAttractionEntity> OrderAttractions { get; set; }
+        public DbSet<OrderCharacterEntity> OrderCharacters { get; set; }
+
+        public LeisureDbContext()
+        {
+        }
+        public LeisureDbContext(DbContextOptions<LeisureDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new CustomerEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new ZoneEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new AttractionEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new CharacterEntityConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderZoneEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderAttractionEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderCharacterEntityConfiguration());
+        }
+        public override DbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ KidsLeisure.DAL/DBContext/LeisureDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace KidsLeisure.DAL.DBContext
+{
+    public class LeisureDbContextFactory : IDesignTimeDbContextFactory<LeisureDbContext>
+    {
+        public LeisureDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LeisureDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=LeisureDb;Trusted_Connection=True;");
+
+            var context = new LeisureDbContext(optionsBuilder.Options);
+            return context;
+        }
+    }
+}

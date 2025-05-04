a6449220682bafd55881d15b13d8dbcbaea6130e
Ristoll <chamata.kristina@gmail.com>
2025-05-04 13:31:16 +0300

    Додано сутності.

---
+++ KidsLeisure.DAL/Entities/OrderAttractionEntity.cs
@@ -0,0 +1,12 @@
+﻿using KidsLeisure.DAL.Interfaces;
+namespace KidsLeisure.DAL.Entities
+{
+    public class OrderAttractionEntity : IOrderItemEntity
+    {
+        public int OrderId { get; set; }
+        public OrderEntity? Order { get; set; }
+
+        public int AttractionId { get; set; }
+        public AttractionEntity? Attraction { get; set; }
+    }
+}
---
+++ KidsLeisure.DAL/Entities/OrderCharacterEntity.cs
@@ -0,0 +1,12 @@
+﻿using KidsLeisure.DAL.Interfaces;
+namespace KidsLeisure.DAL.Entities
+{
+    public class OrderCharacterEntity : IOrderItemEntity
+    {
+        public int OrderId { get; set; }
+        public OrderEntity? Order { get; set; }
+
+        public int CharacterId { get; set; }
+        public CharacterEntity? Character { get; set; }
+    }
+}

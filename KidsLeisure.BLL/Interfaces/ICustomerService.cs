c4d3f2e9392e12ed92a98dcc097e3056c001ae96
Ristoll <chamata.kristina@gmail.com>
2025-05-04 15:14:41 +0300

    Додано сервіс для покупця.

---
+++ KidsLeisure.BLL/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using KidsLeisure.DAL.Entities;
+
+namespace KidsLeisure.BLL.Interfaces
+{
+    public interface ICustomerService
+    {
+        CustomerEntity CurrentCustomer { get; set; }
+        Task<CustomerEntity> CreateCustomerAsync();
+        Task<List<CustomerEntity>> GetAllCustomersAsync();
+        Task<CustomerEntity?> GetCustomerByIdAsync(int customerId);
+    }
+}
---
+++ KidsLeisure.BLL/Services/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿using KidsLeisure.DAL.Entities;
+using KidsLeisure.BLL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using KidsLeisure.DAL.DBContext;
+
+namespace KidsLeisure.BLL.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IRepository<CustomerEntity> _customerRepository;
+        private readonly LeisureDbContext _context;
+
+        public CustomerEntity? CurrentCustomer { get; set; }
+
+        public CustomerService(IRepository<CustomerEntity> customerRepository, LeisureDbContext context)
+        {
+            _customerRepository = customerRepository;
+            _context = context;
+        }
+
+        public async Task<CustomerEntity> CreateCustomerAsync()
+        {
+            await _customerRepository.AddAsync(CurrentCustomer);
+            return CurrentCustomer;
+        }
+
+        public async Task<List<CustomerEntity>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<CustomerEntity?> GetCustomerByIdAsync(int customerId)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(c => c.CustomerId == customerId);
+        }
+    }
+}

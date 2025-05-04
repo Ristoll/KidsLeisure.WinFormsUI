43c5fc33e0f9b376401609898532949a6c26c52a
Ristoll <chamata.kristina@gmail.com>
2025-05-04 14:05:41 +0300

    Заповнення таблиць БД потрібними значеннями.

--- KidsLeisure.WinFormsUI/Program.cs
+++ KidsLeisure.WinFormsUI/Program.cs
@@ -1,17 +1,70 @@
+using KidsLeisure.DAL.Entities;
+using KidsLeisure.DAL.DBContext;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 namespace KidsLeisure.WinFormsUI
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            var host = CreateHostBuilder().Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<LeisureDbContext>();
+
+                if (!context.Zones.Any())
+                {
+                    context.Zones.AddRange(
+                        new ZoneEntity { Name = "Ігрова зона", Price = 200 },
+                        new ZoneEntity { Name = "Спортивна зона", Price = 150 },
+                        new ZoneEntity { Name = "Зона релаксу", Price = 175 },
+                        new ZoneEntity { Name = "Квест-зона", Price = 300 },
+                        new ZoneEntity { Name = "Тематична зона", Price = 290 }
+                    );
+                    context.SaveChanges();
+                }
+
+                if (!context.Attractions.Any())
+                {
+                    context.Attractions.AddRange(
+                        new AttractionEntity { Name = "Батут", Price = 150 },
+                        new AttractionEntity { Name = "Лабіринт", Price = 200 },
+                        new AttractionEntity { Name = "Колесо огляду", Price = 250 },
+                        new AttractionEntity { Name = "Гойдалки", Price = 100 },
+                        new AttractionEntity { Name = "Зимова карусель", Price = 180 }
+                    );
+                    context.SaveChanges();
+                }
+
+                if (!context.Characters.Any())
+                {
+                    context.Characters.AddRange(
+                        new CharacterEntity { Name = "Міккі Маус", Price = 300 },
+                        new CharacterEntity { Name = "Поні Іскорка", Price = 350 },
+                        new CharacterEntity { Name = "Соня з Ведмедиком", Price = 250 },
+                        new CharacterEntity { Name = "Кіт Матроскін", Price = 270 },
+                        new CharacterEntity { Name = "Снігуронька", Price = 400 }
+                    );
+                    context.SaveChanges();
+                }
+            }
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
+        static IHostBuilder CreateHostBuilder() =>
+    Host.CreateDefaultBuilder()
+        .ConfigureServices((hostContext, services) =>
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            services.AddDbContext<LeisureDbContext>(options =>
+                options.UseSqlServer(connectionString));
+        });
     }
 }

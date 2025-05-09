ecabbfed67b528466761fbb25c6efcefefb7648e
Ristoll <chamata.kristina@gmail.com>
2025-05-09 15:22:40 +0300

    Для стратегій обчислення ціни створено базовий клас, щоб зменшити кількість коду та достриматись принципу DRY.

---
+++ KidsLeisure.BLL/Calculator/BaseCalculator.cs
@@ -0,0 +1,64 @@
+﻿using KidsLeisure.BLL.Interfaces;
+using KidsLeisure.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsLeisure.BLL.Calculator
+{
+    public class BaseCalculator
+    {
+        private readonly Lazy<IOrderService> _lazyOrderService;
+
+        public BaseCalculator(Lazy<IOrderService> orderService)
+        {
+            _lazyOrderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+
+        public async Task<decimal> CalculatePriceAsync(OrderEntity order)
+        {
+            var orderService = _lazyOrderService.Value;
+            decimal totalPrice = 0;
+
+            if (order.Zones != null && order.Zones.Count > 0)
+            {
+                foreach (var orderZone in order.Zones.Where(orderZone => orderZone.ZoneId != 0))
+                {
+                    var zone = await orderService.FindItemByIdAsync<ZoneEntity>(orderZone.ZoneId);
+                    if (zone != null)
+                    {
+                        totalPrice += zone.Price;
+                    }
+                }
+            }
+
+            if (order.Attractions != null && order.Attractions.Count > 0)
+            {
+                foreach (var orderAttraction in order.Attractions.Where(orderAttraction => orderAttraction.AttractionId != 0))
+                {
+                    var attraction = await orderService.FindItemByIdAsync<AttractionEntity>(orderAttraction.AttractionId);
+                    if (attraction != null)
+                    {
+                        totalPrice += attraction.Price;
+                    }
+                }
+            }
+
+            if (order.Characters != null && order.Characters.Count > 0)
+            {
+                foreach (var orderCharacter in order.Characters.Where(orderCharacter => orderCharacter.CharacterId != 0))
+                {
+                    var character = await orderService.FindItemByIdAsync<CharacterEntity>(orderCharacter.CharacterId);
+                    if (character != null)
+                    {
+                        totalPrice += character.Price;
+                    }
+                }
+            }
+
+            return totalPrice;
+        }
+    }
+}
--- KidsLeisure.BLL/Calculator/CustomProgramPriceCalculator.cs
+++ KidsLeisure.BLL/Calculator/CustomProgramPriceCalculator.cs
@@ -3,59 +3,19 @@ using KidsLeisure.BLL.Interfaces;
 
 namespace KidsLeisure.BLL.Calculator
 {
-    public class CustomProgramPriceCalculator : IPriceCalculatorStrategy
+    public class CustomProgramPriceCalculator : BaseCalculator, IPriceCalculatorStrategy
     {
-        private readonly Lazy<IOrderService> _lazyOrderService;
         private readonly decimal additionalPrice = 100;
-        public CustomProgramPriceCalculator(Lazy<IOrderService> orderService)
+
+        public CustomProgramPriceCalculator(Lazy<IOrderService> orderService) : base(orderService)
         {
-            _lazyOrderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
         }
 
-        public async Task<decimal> CalculatePriceAsync(OrderEntity order)
+        public new async Task<decimal> CalculatePriceAsync(OrderEntity order)
         {
-            var orderService = _lazyOrderService.Value;
-            decimal totalPrice = 0;
+            decimal totalPrice = await base.CalculatePriceAsync(order);
 
-            if (order.Zones != null && order.Zones.Count > 0)
-            {
-                foreach (var orderZone in order.Zones.Where(orderZone => orderZone.ZoneId != 0))
-                {
-                    var zone = await orderService.FindItemByIdAsync<ZoneEntity>(orderZone.ZoneId);
-                    if (zone != null)
-                    {
-                        totalPrice += zone.Price;
-                    }
-                }
-            }
-
-            if (order.Attractions != null && order.Attractions.Count > 0)
-            {
-                foreach (var orderAttraction in order.Attractions.Where(orderAttraction => orderAttraction.AttractionId != 0))
-                {
-                    var attraction = await orderService.FindItemByIdAsync<AttractionEntity>(orderAttraction.AttractionId);
-                    if (attraction != null)
-                    {
-                        totalPrice += attraction.Price;
-                    }
-                }
-            }
-
-            if (order.Characters != null && order.Characters.Count > 0)
-            {
-                foreach (var orderCharacter in order.Characters.Where(orderCharacter => orderCharacter.CharacterId != 0))
-                {
-                    var character = await orderService.FindItemByIdAsync<CharacterEntity>(orderCharacter.CharacterId);
-                    if (character != null)
-                    {
-                        totalPrice += character.Price;
-                    }
-                }
-            }
-
-            totalPrice += additionalPrice;
-
-            return totalPrice;
+            return totalPrice + additionalPrice;
         }
     }
 }

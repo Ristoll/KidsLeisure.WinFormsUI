d68b0ad0a09055e7eb399e29d417875e1f67e48d
Ristoll <chamata.kristina@gmail.com>
2025-05-14 21:17:10 +0300

    Додано маппери.

---
+++ KidsLeisure.BLL/Mappers/OrderZoneMapper.cs
@@ -0,0 +1,25 @@
+﻿using KidsLeisure.BLL.DTO;
+
+namespace KidsLeisure.BLL.Mappers
+{
+    public static class OrderZoneMapper
+    {
+        public static OrderZoneDto ToDto(DAL.Entities.OrderZoneEntity entity)
+        {
+            return new OrderZoneDto
+            {
+                OrderId = entity.OrderId,
+                ZoneId = entity.ZoneId
+            };
+        }
+
+        public static DAL.Entities.OrderZoneEntity ToEntity(OrderZoneDto dto)
+        {
+            return new DAL.Entities.OrderZoneEntity
+            {
+                OrderId = dto.OrderId,
+                ZoneId = dto.ZoneId
+            };
+        }
+    }
+}

c5cab54dabd074689b5af3f96a944adb4b19b774
Ristoll <chamata.kristina@gmail.com>
2025-05-14 20:40:20 +0300

    Додано клас та інтерфейс UoW

---
+++ KidsLeisure.BLL/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using KidsLeisure.BLL.Interfaces;
+using KidsLeisure.BLL.Repositories;
+using KidsLeisure.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsLeisure.BLL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LeisureDbContext _context;
+        private readonly Dictionary<Type, object> _repositories = new();
+        private bool _disposed = false;
+
+        public UnitOfWork(LeisureDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            if (_repositories.TryGetValue(typeof(T), out var repo))
+                return (IRepository<T>)repo;
+
+            var newRepo = new Repository<T>(_context);
+            _repositories[typeof(T)] = newRepo;
+            return newRepo;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
